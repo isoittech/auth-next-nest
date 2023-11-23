e4cc2948d54fd13d9c7ee7002ef6bc2155f53e05
Tetsuhiro Isomura <isomura@msi.co.jp>
2023-11-23 19:55:54 +0900

    prisma のバージョンを上げたことにともなうビルドエラー対処

--- restapi-todo-nestjs-main/src/auth/auth.service.ts
+++ restapi-todo-nestjs-main/src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, ForbiddenException } from '@nestjs/common';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { Prisma } from '@prisma/client';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
@@ -27,7 +27,7 @@ export class AuthService {
         message: 'ok',
       };
     } catch (error) {
-      if (error instanceof PrismaClientKnownRequestError) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
           throw new ForbiddenException('This email is already taken');
         }
