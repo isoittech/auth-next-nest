d5822b143f36c87edf851931d59b7c79e1eb7f4e
Tetsuhiro Isomura <isomura@msi.co.jp>
2024-01-13 18:50:14 +0900

    backend 起動時にログにポート番号を出すようにした

--- restapi-todo-nestjs-main/src/main.ts
+++ restapi-todo-nestjs-main/src/main.ts
@@ -28,6 +28,11 @@ async function bootstrap() {
       },
     }),
   );
-  await app.listen(process.env.PORT || 3005);
+
+  const port = process.env.PORT || 3005;
+  console.log(`--------------------------------`);
+  console.log(`Backend server port: ${port}`);
+  console.log(`--------------------------------`);
+  await app.listen(port);
 }
 bootstrap();
